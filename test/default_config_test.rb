f2c043f4a41e3425d2e76acf7e7e217a8e0bd99a
Kellen Miller <kellen.miller12@gmail.com>
2026-06-09 21:24:37 -0400

    fix(renovate): use gomod tidy option

--- test/default_config_test.rb
+++ test/default_config_test.rb
@@ -6,14 +6,11 @@ require "minitest/autorun"
 class DefaultConfigTest < Minitest::Test
   CONFIG = JSON.parse(File.read(File.expand_path("../default.json", __dir__)))
 
-  def test_allowed_commands_cover_shared_post_upgrade_tasks
-    commands = CONFIG.fetch("allowedCommands", [])
-
-    assert_includes commands, "^go mod tidy$"
-    assert_includes commands, "^\\./scripts/terraform-locks-update\\.sh$"
+  def test_config_does_not_allow_custom_post_upgrade_commands
+    refute CONFIG.key?("allowedCommands")
   end
 
-  def test_go_module_updates_run_go_mod_tidy
+  def test_go_module_updates_use_builtin_go_mod_tidy
     rule = CONFIG.fetch("packageRules").find do |package_rule|
       package_rule.fetch("matchManagers", []).include?("gomod")
     end
@@ -21,16 +18,7 @@ class DefaultConfigTest < Minitest::Test
     refute_nil rule
     return unless rule
 
-    tasks = rule.fetch("postUpgradeTasks")
-    assert_equal "update", tasks.fetch("executionMode")
-    assert_equal ["go mod tidy"], tasks.fetch("commands")
-    assert_equal "{{{packageFileDir}}}", tasks.fetch("workingDirTemplate")
-    assert_equal({ "golang" => {} }, tasks.fetch("installTools"))
-
-    file_filters = tasks.fetch("fileFilters")
-    assert_includes file_filters, "go.mod"
-    assert_includes file_filters, "go.sum"
-    assert_includes file_filters, "**/go.mod"
-    assert_includes file_filters, "**/go.sum"
+    assert_equal ["gomodTidy"], rule.fetch("postUpdateOptions")
+    refute rule.key?("postUpgradeTasks")
   end
 end
